8c870f6c56191f92be66512618dac47e916d6f88
Mark <mark@webflow.com>
2020-07-14 14:32:51 +0100

    add components and prettier

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
+import Business from '../business/Business'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Business />
       </header>
     </div>
   );
